1f6ec3c10c9c720bbea544d98ec77605def29e6d
tejashinde <tejasshinde@null.net>
2018-10-17 00:39:11 +0530

    Commit

--- LS chmod/lschmod.py
+++ LS chmod/lschmod.py
@@ -1,15 +1,13 @@
-from os import chmod,path
-chmod = 0
-chmods =''
+import re
 for modes in open("ls.txt","r").readlines():
-    print(chmod(path(modes.split(' ')[-1])))
-    # chmod = 0
-    # print(modes.split(' ')[0][1:])
-    # for char in :
-        # if(char == 'r'):
-        #     chmod += 4
-        # elif(char == 'w'):
-        #     chmod += 2
-        # elif(char == 'x'):
-        #     chmod += 1
-    # print(chmod)
+    chmods = ""
+    for mode in re.findall('...',modes.split()[0][1:10]):
+        chmod = 0
+        if mode[0] != "-":
+            chmod += 4
+        if mode[1] != "-":
+            chmod += 2
+        if mode[2] != "-":
+            chmod += 1
+        chmods += str(chmod)
+    print(chmods)
